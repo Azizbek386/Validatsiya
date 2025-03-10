7df816bc9112d72d8bb1e26ac32fae18c70e540c
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-03-10 20:46:31 +0500

    Defined route for user registration

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/', function () {
 });
 
 
+
+
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
 
 Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
