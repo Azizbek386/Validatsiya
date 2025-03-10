8edd0a02ee47604af3a2625a12c69b7e2e6d7e18
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-03-10 20:36:34 +0500

    routes in this file

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
